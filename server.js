0515c14345cce26a32c7cd61eec4bfe437d7f6b9
Eduardo Cavalcante <eduhkva@gmail.com>
2026-01-18 19:22:58 -0300

    Ajuste CORS Socket.io para Render

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
 
 // Configuração inicial
 let currentSenha = 0;
